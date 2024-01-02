4e070742ba53726c4112fa8af184b3f0de4a9780
dbatskel <dmitry.batskel@alertinnovation.com>
2024-01-02 18:31:01 +0100

    [PETB-01] Init project

---
+++ src/test/java/pl/pet/bot/PetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pet.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
