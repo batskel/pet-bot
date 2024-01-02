34f617a0b715cdc61c2676f506e421de68f94996
dbatskel <dmitry.batskel@alertinnovation.com>
2024-01-02 18:58:40 +0100

    [PETB-02] Folder structure

--- src/main/java/pl/pet/bot/PetBotApplication.java
+++ src/main/java/pl/pet/bot/PetBotApplication.java
@@ -3,11 +3,22 @@ package pl.pet.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * PetBotApplication is the main class of the PetBot application.
+ * It is responsible for starting the Spring Boot application.
+ * To start the application, call the main method with appropriate arguments.
+ */
 @SpringBootApplication
 public class PetBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetBotApplication.class, args);
-	}
+  /**
+   * The main method is the entry point of the PetBot application.
+   * It starts the Spring Boot application by invoking the SpringApplication.run() method.
+   *
+   * @param args the command-line arguments
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(PetBotApplication.class, args);
+  }
 
 }
